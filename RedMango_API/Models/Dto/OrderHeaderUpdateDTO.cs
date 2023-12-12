2fb9bbd80a76519fe697be3f478f1edb723f6d5d
Festee <fd61273@ubt-uni.net>
2023-12-12 16:31:41 +0100

    Update Order Details

--- RedMango_API/Models/Dto/OrderHeaderUpdateDTO.cs
+++ RedMango_API/Models/Dto/OrderHeaderUpdateDTO.cs
@@ -15,7 +15,7 @@ namespace RedMango_API.Models.Dto
         public string PickupEmail { get; set; }
 
 
-        public DateTime OrderDate { get; set; }
+        
         public string StripePaymentIntentId { get; set; }
         public string Status { get; set; }
        
